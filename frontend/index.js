05a9e0a382a7714c28049359491114b04f0278a1
Mindjet <pearl920@outlook.com>
2019-11-17 22:37:19 +0800

    [*]: create frontend dir

---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Home from './src/components/Home'
+import './src/styles/Global.scss'
+
+ReactDOM.render(<Home />, document.getElementById('root'))
--- index.js
+++
@@ -1,4 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-
-ReactDOM.render(document.getElementById('root'), <p>'Hello React'</p>)
